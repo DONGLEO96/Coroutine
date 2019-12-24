dfc826be3f71f4e9417b6a4e2083b192817aae96
DongL <48015657+DONGLEO96@users.noreply.github.com>
2019-12-24 11:42:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@ void func3(CoManager* c)
 	c->coyield();
 	cout << "func3" << endl;
 }
+void func5(CoManager* c)
+{
+	cout << "func5" << endl;
+	c->coyield();
+	cout << "func5" << endl;
+}
 void func1(CoManager* c)
 {
 	cout << "func1" << endl;
@@ -23,15 +29,25 @@ void func2(CoManager* c)
 	cout << "func2" << endl;
 	c->coyield();
 	cout << "func2" << endl;
-	CoManager c2;
-	int func3id=c2.cocreate(std::bind(func3, &c2));
-	c2.coresume(func3id);
+	int coid = c->cocreate(std::bind(func3, c));
+	c->coresume(coid);
 	cout << "func2" << endl;
-	c2.coresume(func3id);
-	cout << "func2" << endl;
+	int coid2 = c->cocreate(std::bind(func5, c));
+	c->coresume(coid2);
+	cout << "main2" << endl;
+	c->coresume(coid);
+	cout << "main2" << endl;
+	c->coresume(coid2);
+	cout << "0000" << endl;
+
+
 }
 void func4(CoManager* c)
 {
+	cout << "func4" << endl;
+	c->coyield();
+	cout << "func4" << endl;
+	c->coyield();
 	cout << "func4" << endl;
 }
 
@@ -46,9 +62,14 @@ int main()
 	cout << "main" << endl;
 	c.coresume(id2);
 	cout << "main" << endl;
-	c.coresume(id1);
+	c.coresume(id3);
 	cout << "main" << endl;
 	c.coresume(id2);
+	cout << "main" << endl;
+	
+	c.coresume(id1);
+	cout << "main" << endl;
+	c.coresume(id3);
 	cout << "main" << endl;
 	c.coresume(id1);
 	cout << "main" << endl;
